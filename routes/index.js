f4d0f1fcdd0b35f3830324c97cb62d7741a8bee6
Anand Chandran <anchandran@FWS-CHE-LT-3082.local>
2019-12-07 17:32:09 +0530

    db conncetion and hosting implemented on mongoDB.Atlas

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,14 @@
-const express = require('express');
-const router =  express.Router();
-var bodyParser = require('body-parser');
+const express = require("express");
+const router = express.Router();
+var bodyParser = require("body-parser");
 
-const controller = require('../controllers/index');
+const controller = require("../controllers/index");
 
-
-router.post('/api/tasks/create',controller.addTodo);
-router.post('/api/sub_tasks/create/:parent_id',controller.addSubTodo);
-router.put('/api/tasks/update/:id',controller.updateTodo);
-router.get('/api/tasks/show/:id',controller.showTodo);
-router.delete('/api/tasks/delete/:id',controller.deleteTodo);
-
+router.post("/api/tasks/create", controller.addTodo);
+router.post("/api/sub_tasks/create/:parent_id", controller.addSubTodo);
+router.put("/api/tasks/update/:id", controller.updateTodo);
+router.get("/api/tasks/show/:id", controller.showTodo);
+router.get("/api/tasks/show", controller.showAll);
+router.delete("/api/tasks/delete/:id", controller.deleteTodo);
 
 module.exports = router;
